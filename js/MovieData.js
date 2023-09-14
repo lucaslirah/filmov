c97be9ec14aad9b1fa16113f3717be4c5852317f
Lucas Lira <lucasliraone@gmail.com>
2023-09-14 18:34:20 -0300

    Update MovieData.js - HTTP to HTTPS

--- js/MovieData.js
+++ js/MovieData.js
@@ -1,13 +1,13 @@
 // const app = document.querySelector('#app')
 // const p = document.createElement('p')
-// p.textContent = `poha`
+// p.textContent = `content`
 // app.appendChild(p)
 
 export class MovieData{
   static search(title){
     const apiKey = '22fddf5e'
 
-    const endpoint = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`
+    const endpoint = `https://www.omdbapi.com/?t=${title}&apikey=${apiKey}`
 
     return fetch(endpoint)
     .then(data => data.json())
